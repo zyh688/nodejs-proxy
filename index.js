f34ec3232d8fa5892814a1da3c8f6b4707088859
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-07-16 05:40:42 +0700

    1

--- index.js
+++ index.js
@@ -5,32 +5,48 @@
  * @returns {void}
  */
 module.exports = function (port, uuid) {
-  const net = require('net');
-  const { WebSocket, createWebSocketStream } = require('ws');
-  const logcb = (...args) => console.log.bind(this, ...args);
-  const errcb = (...args) => console.error.bind(this, ...args);
-  // 使用传入的 port 和 uuid 替换默认值 env var 优先级最高
-  uuid = (process.env.UUID || uuid || 'd342d11e-d424-4583-b36e-524ab1f0afa4').replaceAll('-', '');
-  port = process.env.PORT || port || 7860;
-  const wss = new WebSocket.Server({ port }, logcb('listen:', port));
-  wss.on('connection', ws => {
-    ws.once('message', msg => {
-      const [VERSION] = msg;
-      const id = msg.slice(1, 17);
-      if (!id.every((v, i) => v == parseInt(uuid.substr(i * 2, 2), 16))) return;
-      let i = msg.slice(17, 18).readUInt8() + 19;
-      const port = msg.slice(i, i += 2).readUInt16BE(0);
-      const ATYP = msg.slice(i, i += 1).readUInt8();
-      const host = ATYP == 1 ? msg.slice(i, i += 4).join('.') ://IPV4
-        (ATYP == 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) ://domain
-          (ATYP == 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : ''));//ipv6
-      logcb('conn:', host, port);
-      ws.send(new Uint8Array([VERSION, 0]));
-      const duplex = createWebSocketStream(ws);
-      net.connect({ host, port }, function () {
-        this.write(msg.slice(i));
-        duplex.on('error', errcb('E1:')).pipe(this).on('error', errcb('E2:')).pipe(duplex);
-      }).on('error', errcb('Conn-Err:', { host, port }));
-    }).on('error', errcb('EE:'));
+  // Import required modules
+  const net = require("net");
+  const { WebSocket, createWebSocketStream } = require("ws");
+  // Prepare UUID and Port
+  const UUID = (process.env.UUID || uuid || 'd342d11e-d424-4583-b36e-524ab1f0afa4').replaceAll('-', '');
+  const PORT = process.env.PORT || port || 7860;
+  // 创建一个 WebSocket 服务器
+  const server = new WebSocket.Server({ port: PORT }, () => {
+    console.log(`Server is listening on port: ${PORT}`);
+  });
+  // 通过参数启动你的应用
+  server.on("connection", (ws) => {
+    ws.once("message", (msg) => {
+      let prefix = msg[0],
+        uuid = msg.slice(1, 17),
+        l = msg.slice(17, 18).readUInt8() + 19,
+        remotePort = msg.slice(l, l += 2).readUInt16BE(0),
+        remoteAddressType = msg.slice(l, l += 1).readUInt8(),
+        remoteAddress;
+
+      if (!uuid.every((val, i) => val == parseInt(UUID.substr(2 * i, 2), 16)))
+        return console.error("Invalid UUID");
+
+      if (remoteAddressType == 1) {
+        remoteAddress = msg.slice(l, l += 4).join(".");
+      } else if (remoteAddressType == 2) {
+        remoteAddress = new TextDecoder().decode(msg.slice(l + 1, l += 1 + msg.slice(l, l + 1).readUInt8()));
+      } else if (remoteAddressType == 3) {
+        remoteAddress = msg.slice(l, l += 16).reduce((str, byte, i, bytes) => i % 2 ? str.concat(bytes.slice(i - 1, i + 1)) : str, []).map(bytes => bytes.readUInt16BE(0).toString(16)).join(":");
+      } else {
+        remoteAddress = "";
+      }
+
+      console.log(`A new connection: ${remoteAddress}:${remotePort}`);
+      ws.send(new Uint8Array([prefix, 0]));
+
+      let wsStream = createWebSocketStream(ws);
+      net.connect({ host: remoteAddress, port: remotePort }, function () {
+        this.write(msg.slice(l));
+        wsStream.on("error", () => console.error("WebSocket Stream Error")).pipe(this).pipe(wsStream);
+      }).on('error', () => console.error('TCP Connection error:', { host: remoteAddress, port: remotePort }));
+    }).on("error", () => console.error("WebSocket Connection Error"));
   });
 };
+// this code used GPT-4 to generate
