2540c23dd6dbb9edc71f8d9433bd0b3fa58b8888
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-07-16 06:36:22 +0700

    remove useless package

--- test.js
+++ test.js
@@ -1,43 +1,8 @@
-// Import required modules
-const net = require("net");
-const { WebSocket, createWebSocketStream } = require("ws");
-// Prepare UUID and Port
-const UUID = (process.env.UUID || 'd342d11e-d424-4583-b36e-524ab1f0afa4').replaceAll('-', '');
-const PORT = process.env.PORT || 7860;
+const createVLESSServer = require('./index.js');
 
-const server = new WebSocket.Server({ port: PORT }, () => {
-    console.log(`Server is listening on port: ${PORT}`);
-});
+// 定义端口和 UUID
+const port = 3001;
+const uuid = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-server.on("connection", (ws) => {
-    ws.once("message", (msg) => {
-        let prefix = msg[0],
-            uuid = msg.slice(1, 17),
-            l = msg.slice(17, 18).readUInt8() + 19,
-            remotePort = msg.slice(l, l += 2).readUInt16BE(0),
-            remoteAddressType = msg.slice(l, l += 1).readUInt8(),
-            remoteAddress;
-
-        if (!uuid.every((val, i) => val == parseInt(UUID.substr(2 * i, 2), 16)))
-            return console.error("Invalid UUID");
-
-        if (remoteAddressType == 1) {
-            remoteAddress = msg.slice(l, l += 4).join(".");
-        } else if (remoteAddressType == 2) {
-            remoteAddress = new TextDecoder().decode(msg.slice(l + 1, l += 1 + msg.slice(l, l + 1).readUInt8()));
-        } else if (remoteAddressType == 3) {
-            remoteAddress = msg.slice(l, l += 16).reduce((str, byte, i, bytes) => i % 2 ? str.concat(bytes.slice(i - 1, i + 1)) : str, []).map(bytes => bytes.readUInt16BE(0).toString(16)).join(":");
-        } else {
-            remoteAddress = "";
-        }
-
-        console.log(`A new connection: ${remoteAddress}:${remotePort}`);
-        ws.send(new Uint8Array([prefix, 0]));
-
-        let wsStream = createWebSocketStream(ws);
-        net.connect({ host: remoteAddress, port: remotePort }, function () {
-            this.write(msg.slice(l));
-            wsStream.on("error", () => console.error("WebSocket Stream Error")).pipe(this).pipe(wsStream);
-        }).on('error', () => console.error('TCP Connection error:', { host: remoteAddress, port: remotePort }));
-    }).on("error", () => console.error("WebSocket Connection Error"));
-});
+// 调用函数启动 VLESS 服务器
+createVLESSServer(port, uuid);
