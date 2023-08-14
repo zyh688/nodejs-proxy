9b61885f0127dd7a532807bfe48c47763b2b9a8f
zyh688 <66469155+zyh688@users.noreply.github.com>
2023-08-14 12:53:07 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const createVLESSServer = require('./index.js');
 
 // 定义端口和 UUID
 const port = 3001;
-const uuid = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+const uuid = '890d0681-ebf8-40d2-82fa-7133e0e58c87';
 
 // 调用函数启动 VLESS 服务器
 createVLESSServer(port, uuid);
